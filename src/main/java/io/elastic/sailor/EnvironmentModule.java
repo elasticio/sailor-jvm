ca323a14c1ed96ec644b4617d33fab38411275a7
Igor Drobiazko <igor@elastic.io>
2015-08-27 13:04:06 +0200

    Added missing env var definitions

--- src/main/java/io/elastic/sailor/EnvironmentModule.java
+++ src/main/java/io/elastic/sailor/EnvironmentModule.java
@@ -18,6 +18,16 @@ public class EnvironmentModule extends AbstractModule {
         bindRequiredStringEnvVar(Constants.ENV_VAR_TASK);
         bindRequiredStringEnvVar(Constants.ENV_VAR_STEP_ID);
 
+        bindRequiredStringEnvVar(Constants.ENV_VAR_AMQP_URI);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_LISTEN_MESSAGES_ON);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_PUBLISH_MESSAGES_TO);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_DATA_ROUTING_KEY);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_ERROR_ROUTING_KEY);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_REBOUND_ROUTING_KEY);
+        bindRequiredStringEnvVar(Constants.ENV_VAR_SNAPSHOT_ROUTING_KEY);
+
+        bindRequiredStringEnvVar(Constants.ENV_VAR_POST_RESULT_URL);
+
 
         // optional env vars
         bindOptionalIntegerEnvVar(
