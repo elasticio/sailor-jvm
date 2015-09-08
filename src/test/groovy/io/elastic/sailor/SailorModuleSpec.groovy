3c431c9c73b223dec6198d0a4de6234af5d60f90
Igor Drobiazko <igor@elastic.io>
2015-09-08 14:29:59 +0200

    Added a test

---
+++ src/test/groovy/io/elastic/sailor/SailorModuleSpec.groovy
@@ -0,0 +1,27 @@
+package io.elastic.sailor
+
+import com.google.gson.JsonObject
+import com.google.inject.Guice
+import com.google.inject.Injector
+import com.google.inject.Key
+import com.google.inject.name.Names
+import spock.lang.Specification
+
+class SailorModuleSpec extends Specification {
+
+    Injector injector;
+
+    def setup() {
+       injector = Guice.createInjector(new SailorModule(), new SailorTestModule())
+    }
+
+
+
+    def "it should provide configuration"() {
+        when:
+        def cfg = injector.getInstance(Key.get(JsonObject.class, Names.named(Constants.NAME_CFG_JSON)));
+
+        then:
+        cfg.toString() == '{"uri":"546456456456456"}'
+    }
+}
