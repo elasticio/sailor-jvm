6cec5397334456ec3a22101ddb188a10927fd444
Igor Drobiazko <igor@elastic.io>
2017-03-29 10:34:21 +0200

    Removed temporal printing of msg to stdout

--- src/main/java/io/elastic/sailor/impl/DataCallback.java
+++ src/main/java/io/elastic/sailor/impl/DataCallback.java
@@ -38,8 +38,6 @@ public class DataCallback extends CountingCallbackImpl {
 
         final JsonObject messageAsJson = executionContext.createPublisheableMessage(message);
 
-        logger.info("Publishing message {}", messageAsJson);
-
         // encrypt
         byte[] encryptedPayload = cipher.encrypt(JSON.stringify(messageAsJson)).getBytes();
 
